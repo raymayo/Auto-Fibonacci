8e54ecac89ae132e5a5467835af5a3380c2a31c4
ray <ormillada.jr@gmail.com>
2021-10-12 14:26:32 +0800

    change the math

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ let result = document.getElementById("result");
 submit.addEventListener("click", function () {
   let a = document.getElementById("a").value;
   let b = document.getElementById("b").value;
-  let res1 = a / b;
-  let res2 = (a + b) / a;
-  result.innerText = `${res1} & ${res2}`;
+  let res1 = +a / +b;
+  let res2 = +a + +b;
+  let res3 = res2 / +a;
+  result.innerText = `${res1} & ${res3}`;
 });
